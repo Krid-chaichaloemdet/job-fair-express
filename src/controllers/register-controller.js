a97e40a66e9351c1b8765146af4451e6ed471e8d
Krid-Chaichaloemdet <kridtue01@hotmail.com>
2024-11-14 17:00:52 +0700

    go home

--- src/controllers/register-controller.js
+++ src/controllers/register-controller.js
@@ -5,7 +5,7 @@ exports.register = async (req, res, next) => {
     const timeStamp = Date.now();
     const { email, phoneNumber } = req.body;
     req.body.createdAt = "" + timeStamp;
-
+    console.log(req.body);
     const isMatchEmail = await prisma.user.findFirst({
       where: {
         email,
@@ -29,6 +29,105 @@ exports.register = async (req, res, next) => {
     });
 
     res.status(201).json({ message: "created" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.createTestRecord = async (req, res, next) => {
+  try {
+    const timeStamp = Date.now();
+    // console.log(req.body);
+    const {
+      email,
+      phoneNumber,
+      score,
+      totalClick,
+      totalSkip,
+      totalTime,
+      testScore,
+      testTime,
+      test1,
+      test2,
+      test3,
+      test4,
+      test5,
+      test6,
+      test7,
+      test8,
+    } = req.body;
+    req.body.createdAt = "" + timeStamp;
+    const isMatchEmail = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (isMatchEmail) {
+      const picTimes = [];
+      for (let i = 1; i <= 30; i++) {
+        const picTime = req.body[`pic${i}`];
+        picTimes.push(picTime);
+      }
+      const clickCounts = [];
+      for (let i = 1; i <= 30; i++) {
+        const clickCount = req.body[`click${i}`];
+        clickCounts.push(clickCount);
+      }
+      const scoreCounts = [];
+      for (let i = 1; i <= 30; i++) {
+        const scoreCount = req.body[`isCorrect${i}`];
+        scoreCounts.push(scoreCount);
+      }
+
+      const skipCounts = [];
+      for (let i = 1; i <= 30; i++) {
+        const skipCount = req.body[`skip${i}`];
+        skipCounts.push(skipCount);
+      }
+
+      await prisma.differ.create({
+        data: {
+          userId: +isMatchEmail.userId,
+          timeCounts: JSON.stringify(picTimes),
+          clickCounts: JSON.stringify(clickCounts),
+          scoreCounts: JSON.stringify(scoreCounts),
+          skipCounts: JSON.stringify(skipCounts),
+          createdAt: "" + timeStamp,
+          score: "" + score,
+          totalClick: +totalClick,
+          totalSkip: +totalSkip,
+          totalTime: "" + totalTime,
+        },
+      });
+
+    const testRes =   await prisma.test.create({
+        data: {
+          userId: +isMatchEmail.userId,
+          testScore: "" + testScore,
+          testTime: "" + testTime,
+          test1,
+          test2,
+          test3,
+          test4,
+          test5,
+          test6,
+          test7,
+          test8,
+          createdAt: "" + timeStamp,
+        },
+      });
+      console.log("testRes",testRes)
+      return res.status(200).json({ message: "created success" });
+    }
+
+    const isMatchPhoneNumber = await prisma.user.findFirst({
+      where: {
+        phoneNumber,
+      },
+    });
+    if (isMatchPhoneNumber) {
+      return res.status(200).json({ message: "registation success" });
+    }
   } catch (error) {
     console.log(error);
   }
--- src/router/user-route.js
+++ src/router/user-route.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 const userController = require('../controllers/register-controller')
-
+const createTestRecordController = require('../controllers/register-controller')
 router.post('/register' ,userController.register)
-
+router.post('/createTestRecord',createTestRecordController.createTestRecord)
 
 module.exports = router
